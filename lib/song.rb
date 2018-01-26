ffb7930a106c5c2f643b61ac62fae16714ac81a0
Sara Tibbetts <sxt4866@rit.edu>
2018-01-26 16:34:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
   def self.new_by_filename(filename)
     filename = filename.gsub(".mp3", "")
     parts = filename.split(" - ")
-    artist_name = parts[1]
-    song_name = parts[0]
+    artist_name = parts[0]
+    song_name = parts[1]
     genre_name = parts[2]
 
     artist = Artist.new(artist_name)
